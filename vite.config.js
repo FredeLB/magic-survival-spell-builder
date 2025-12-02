227ac5ffbdcb4606e4c9edf1b93857963cc38dec
frede-mg <frede@lesmauvaisgarcons.ca>
2025-12-02 17:34:31 -0500

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,30 @@ import { resolve } from 'path'
 
 export default {
     root: resolve(__dirname, 'src'),
-    base: '/',
+    base: process.env.NODE_ENV === 'production' 
+        ? '/magic-survival-spell-builder/' 
+        : '/',
     build: {
         outDir: resolve(__dirname, 'dist'),
-        emptyOutDir: true, // This enables emptying
+        emptyOutDir: true,
+        // Add this for proper asset handling
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                // Versioned asset names
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1);
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'img';
+                    } else if (/woff2?/i.test(extType)) {
+                        extType = 'fonts';
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+            }
+        }
     },
     server: {
         base: '/magic-survival-spell-builder/',
